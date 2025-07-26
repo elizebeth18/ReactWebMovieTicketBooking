8666f99f8e13dbd949cfabda43ea9bf00658b3b7
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-07-26 18:47:44 +0530

    add cleanup function in useEffect method and abortController in createAsync Thunk

--- src/Components/MovieBookingUI.jsx
+++ src/Components/MovieBookingUI.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
-import { AppBar, Link, Toolbar, Tabs, Tab, TextField, IconButton, Grid, Card, CardContent, Button, Typography, Box, Container } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Tabs, Tab, Box } from '@mui/material';
 import NavBar from './Home/NavBar';
 import MovieSlider from './Home/MovieSlider';
+import { fetchMoviesList, selectMoviesList } from '../store/movieSlice';
+import RecommendedMovies from './Recommended Movies';
 
 export default function MovieBookingUI() {
+
     const [value, setValue] = useState(0);
+    const [moviesList, setMoviesList] = useState([]);
+    const dispatch = useDispatch();
+    const moviesFromStore = useSelector(selectMoviesList);
+
+
+    useEffect(() => {
+        const fetchAction = dispatch(fetchMoviesList());
+
+        return () => {
+            fetchAction.abort(); //cancels the request if unmounted early
+        };
+
+    }, [dispatch]);
+
+    useEffect(() => {
+        moviesFromStore ? setMoviesList(moviesFromStore) : dispatch(fetchMoviesList())
+    }, [moviesFromStore]);
 
     return (
         <>
@@ -12,36 +33,17 @@ export default function MovieBookingUI() {
 
                 <NavBar />
 
-
                 <Tabs value={value} onChange={(e, newValue) => setValue(newValue)} centered>
                     <Tab key="1" label="Latest Movies" sx={{ border: '1px solid black', width: '33.33%' }} />
                     <Tab key="2" label="Upcoming Movies" sx={{ border: '1px solid black', width: '33.33%', margin: '0 5px' }} />
                     <Tab key="3" label="Nearby Events" sx={{ width: '33.33%', border: '1px solid black' }} />
                 </Tabs>
 
+                <MovieSlider moviesList={moviesList} />
 
-                <MovieSlider />
-
-                {/* 🎥 Recommended Movies */}
-                <Box sx={{ mt: 5 }}>
-                    <Typography variant="h5" align="center" mb={1}>Recommended Movies</Typography>
-                    <Grid container spacing={3} justifyContent="center">
-                        {['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4'].map((movie) => (
-                            <Grid key={Math.random(1)} xs={10} sm={6} md={3}>
-                                <Card sx={{ p: 2, textAlign: "center" }}>
-                                    <Typography variant="subtitle1">{movie}</Typography>
-                                    <Button variant="outlined" sx={{ mt: 2 }}>Book</Button>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-
-
-
+                <RecommendedMovies moviesList={moviesList} />
 
             </Box>
-
         </>
     );
 }
--- src/store/movieSlice.jsx
+++ src/store/movieSlice.jsx
@@ -7,8 +7,20 @@ const initialState = {
     error: null
 };
 
-export const fetchMoviesList = createAsyncThunk('movie/fetch', async () => {
-    const response = await axios.get('http://localhost:9112/movies');
+export const fetchMoviesList = createAsyncThunk('movie/fetch', async (_, { signal }) => {
+
+    //'_' in the async function means ignore if no payload to pass
+    const controller = new AbortController();
+
+    //if the signal from RTK aborts, cancel the controller
+    signal.addEventListener('abort', () => {
+        controller.abort(); //will cancel the axios request
+    });
+
+    const response = await axios.get('http://localhost:9112/movies', {
+        signal: controller.signal
+    });
+    
     return response.data;
 })
 
@@ -18,19 +30,19 @@ const movieSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(fetchMoviesList.pending, state => {
-            state.isLoading = true;
-            state.movies = [];
-            state.error = null
-        })
-        .addCase(fetchMoviesList.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.moviesList = action.payload;
-        })
-        .addCase(fetchMoviesList.rejected, (state,action) => {
-            state.isLoading = false;
-            state.error = action.error?.message;
-        })
+            .addCase(fetchMoviesList.pending, state => {
+                state.isLoading = true;
+                state.movies = [];
+                state.error = null
+            })
+            .addCase(fetchMoviesList.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.moviesList = action.payload;
+            })
+            .addCase(fetchMoviesList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error?.message;
+            })
     }
 });
 
