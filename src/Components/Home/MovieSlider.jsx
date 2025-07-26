7f0cb18b9b48c8a9550cf9f6d649781968a1117b
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-07-26 16:27:21 +0530

    fix horizontal and vertical scroll

--- src/Components/Home/MovieSlider.jsx
+++ src/Components/Home/MovieSlider.jsx
@@ -7,29 +7,6 @@ import 'slick-carousel/slick/slick-theme.css';
 import { selectMoviesList, fetchMoviesList } from '../../store/movieSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
-const movies = [
-    {
-        title: "Thor: Ragnarok",
-        image: "https://image.tmdb.org/t/p/w500/zB4G3D0WcaaXOrX1D8oWnBCXxvH.jpg",
-        genre: "Action, Comedy"
-    },
-    {
-        title: "Avengers: Endgame",
-        image: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
-        genre: "Action, Sci-Fi"
-    },
-    {
-        title: "The Dark Knight",
-        image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-        genre: "Crime, Drama"
-    },
-    {
-        title: "Interstellar",
-        image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
-        genre: "Sci-Fi, Drama"
-    }
-];
-
 const MovieSlider = () => {
     const [moviesList, setMoviesList] = useState([]);
     const dispatch = useDispatch();
@@ -40,7 +17,7 @@ const MovieSlider = () => {
         speed: 600,
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: true,
         responsive: [
             {
                 breakpoint: 900,
@@ -58,7 +35,7 @@ const MovieSlider = () => {
     };
 
     useEffect(() => {
-       moviesFromStore ? setMoviesList(moviesFromStore): dispatch(fetchMoviesList);
+        moviesFromStore ? setMoviesList(moviesFromStore) : dispatch(fetchMoviesList);
     }, [moviesFromStore])
 
     useEffect(() => {
@@ -67,15 +44,12 @@ const MovieSlider = () => {
 
     return (
         <Box sx={{ width: '65%', mx: 'auto', my: 2 }}>
-            <Typography variant='h6' textAlign='center'>
-                Now Showing
-            </Typography>
             <Slider {...settings}>
                 {moviesList && moviesList.map((movie, index) => {
                     return (
-                        <Box key={index} px={2}>
-                            <Card sx={{ borderRadius: 3 }}>
-                                <CardMedia component="img"
+                        <Box key={index} px={1}>
+                            <Card sx={{ width: 320, borderRadius: 3 }}>
+                                <CardMedia sx={{ objectFit: "cover" }} component="img"
                                     height="250" alt={movie.title}
                                     image={movie.image} />
                                 <CardContent>
--- src/Components/MovieBookingUI.jsx
+++ src/Components/MovieBookingUI.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
-import { AppBar, Toolbar, Tabs, Tab, TextField, IconButton, Grid, Card, CardContent, Button, Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Link, Toolbar, Tabs, Tab, TextField, IconButton, Grid, Card, CardContent, Button, Typography, Box, Container } from '@mui/material';
 import NavBar from './Home/NavBar';
 import MovieSlider from './Home/MovieSlider';
 
 export default function MovieBookingUI() {
+    const [value, setValue] = useState(0);
+
     return (
-        <Box sx={{ width: '100vw' }}>
+        <>
+            <Box sx={{ width: "100vw", bgcolor: "#eac6f3", overflow: 'hidden' }}>
 
-            <NavBar />
+                <NavBar />
 
 
-            <Tabs centered>
-                <Tab label="Latest Movies" sx={{ border: '1px solid black', width: '33.33%' }} />
-                <Tab label="Upcoming Movies" sx={{ border: '1px solid black', width: '33.33%', margin: '0 5px' }} />
-                <Tab label="Nearby Events" sx={{ width: '33.33%', border: '1px solid black' }} />
-            </Tabs>
+                <Tabs value={value} onChange={(e, newValue) => setValue(newValue)} centered>
+                    <Tab key="1" label="Latest Movies" sx={{ border: '1px solid black', width: '33.33%' }} />
+                    <Tab key="2" label="Upcoming Movies" sx={{ border: '1px solid black', width: '33.33%', margin: '0 5px' }} />
+                    <Tab key="3" label="Nearby Events" sx={{ width: '33.33%', border: '1px solid black' }} />
+                </Tabs>
 
-            
-            <MovieSlider />
 
-            {/* 🎥 Recommended Movies */}
-            <Box sx={{ textAlign: 'center', mt: 4 }}>
-                <Typography variant="h6">Recommended Movies</Typography>
-                <Grid container spacing={2} justifyContent="center" sx={{ mt: 2 }}>
-                    {['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4'].map((movie, index) => (
-                        <Grid item key={index}>
-                            <Card sx={{ width: 150, height: 200 }}>
-                                <CardContent sx={{ textAlign: 'center' }}>
-                                    <Typography>{movie}</Typography>
-                                    <Button variant="outlined" sx={{ mt: 4 }}>Book</Button>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
+                <MovieSlider />
+
+                {/* 🎥 Recommended Movies */}
+                <Box sx={{ mt: 5 }}>
+                    <Typography variant="h5" align="center" mb={1}>Recommended Movies</Typography>
+                    <Grid container spacing={3} justifyContent="center">
+                        {['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4'].map((movie) => (
+                            <Grid key={Math.random(1)} xs={10} sm={6} md={3}>
+                                <Card sx={{ p: 2, textAlign: "center" }}>
+                                    <Typography variant="subtitle1">{movie}</Typography>
+                                    <Button variant="outlined" sx={{ mt: 2 }}>Book</Button>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+
+
+
+
             </Box>
-        </Box>
+
+        </>
     );
 }
